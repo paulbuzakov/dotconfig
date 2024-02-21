c592a62a1dc6cec5eb7fb34dc02bfc2dac2a970f
paulbuzakov <paulbuzakov@gmail.com>
2024-02-22 00:10:49 +0500

    feat: Changing colorscheme

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -1,10 +1,12 @@
 return {
   { "olivercederborg/poimandres.nvim" },
+  { "craftzdog/solarized-osaka.nvim" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "poimandres",
+      -- colorscheme = "poimandres",
+      colorscheme = "solarized-osaka",
     },
   },
 }
